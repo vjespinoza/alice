6cbe0a855b9f120059621ca70b02ad6e8f3508c6
vjespinoza <vjespinoza84@gmail.com>
2020-09-04 19:39:01 -0500

    js stop scroll when mobile menu active

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-$(document).ready(function () {
-  $("#toggle_mob-menu").click(function () {
-    lockScroll();
-  });
+//Prevents body scrolling when mobile menu is active.
+const checkbox = document.getElementById("toggle_mob-menu");
+
+checkbox.addEventListener("change", (event) => {
+  if (event.target.checked) {
+    body.classList.add("stop_scroll");
+  } else {
+    body.classList.remove("stop_scroll");
+  }
 });
 
-function lockScroll() {
-  if ($("#nav_bar").is(":visible")) {
-    $("body").addClass("stop_scroll");
-  } else {
-    $("body").removeClass("stop_scroll");
-  }
-}
-
-// I HAVE NO IDEA WHAT I'M DOING!!!!!!!! =(
+//This solution creates an issue:
+//When the mobile menu is closed without "unchecking" the checkbox
+//the .stop_scroll class remians active, preventing the body to scroll
+//when the mobile menu is not visible (active).
